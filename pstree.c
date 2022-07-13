62ee016db9c0f5007c8ad5d55c9241b8c3eeb969
ysy <929428294@qq.com>
2022-07-14 02:02:25 +0800

    has done step2&3

--- pstree.c
+++ pstree.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-
-struct pidinfo{
-	char *name;
+#include <dirent.h>  
+#include <sys/stat.h>
+#include <stdlib.h>
+typedef struct pidinfo{
+	char name[40];
 	__pid_t pid;
 	__pid_t ppid;
-};
-
-
+}PidInfo;
+PidInfo pidinfos[10000];
+int pid_count=0;
 char* readcmdops(int argc, char *argv[])//get commandline ops
 {
 	assert(argc>=1);
@@ -32,9 +34,59 @@ char* readcmdops(int argc, char *argv[])//get commandline ops
 	strcpy(p,ops);
 	return p;
 }
-int getppid(char *path)
+pid_t readprocessname_ppid(__pid_t pid,char name[])
 {
-
+	char *str=(char *)malloc(sizeof(char)*20);
+	sprintf(str,"%d",pid);
+	char *pidpath=strcat("/proc/",str);
+	pidpath=strcat(pidpath,"/stat");
+	FILE *fp=fopen(pidpath,"r");
+	if (fp){
+		char i;
+		__pid_t _pid,ppid;
+		char pname[40];
+		fscanf(fp,"%d (%s %c %d",&_pid,pname,&i,&ppid);
+		strncpy(name,pname,strlen(pname)-1);
+		name[strlen(name)]='\0';
+		assert(pid==_pid);
+		fclose(fp);
+		free(str);
+		return ppid;
+	}else{
+		printf("open failed");
+		free(str);
+		return NULL;
+	}
+}
+void setProcessInfo()
+{
+	// read /proc/
+	DIR *dp;
+	//struct stat *statbuf;
+	if(dp=opendir("/proc/")==NULL)
+	{
+		fprintf(stderr,"%s","Can 't open /proc/");
+		return;
+	}
+	else
+	{
+		int pid=0;
+		struct dirent *entry;
+		while ((entry=readdir(dp))!=NULL)
+		{
+			if((pid=atoi(entry->d_name))==0)
+			{
+				continue;
+			}
+			else{// store in pidinfo (name and pid and ppid)
+				pidinfos[pid_count].pid=pid;
+				pidinfos[pid_count].ppid=readprocessname_ppid(pid,pidinfos[pid_count].name);
+				assert(pidinfos[pid_count].ppid>-1);
+				pid_count++;
+				// TODO getname and ppid
+			}
+		}
+	}
 }
 
  //   /proc/pid/stat 文件中有name和ppid
